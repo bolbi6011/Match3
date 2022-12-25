786966739c860ec5136ac2f3cf69d25778ba44c5
OYovenko <sempaikakachihatake@gmail.com>
2022-12-25 16:59:57 +0200

    2 comit
    2 comit

---
+++ My project/Assets/Scripts/Board.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public static Board instance;
+
+    private int xSize, ySize;
+    private Tile tileGO;
+    private List<Sprite> tileSprite = new List<Sprite>();
+
+    private void Awake()
+    {
+        instance= this;
+    }
+    public void SetValue(int xSize, int ySize, Tile tileGO, List<Sprite> tileSprite)
+    {
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.tileGO = tileGO;
+        this.tileSprite = tileSprite;
+    }
+    public void CreateBoard()
+    {
+
+    }
+}
---
+++ My project/Assets/Scripts/GameManeger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+ public class BoardSetting
+{
+    public int xSize, ySize;
+    public Tile tileGo;
+    public List<Sprite> tileSprite;
+}
+
+public class GameManeger : MonoBehaviour
+
+{
+    [Header("Параметри игровой доски")]
+    public BoardSetting BoardSetting;
+
+    private void Start()
+    {
+        Board.instance.SetValue(BoardSetting.xSize,BoardSetting.ySize,BoardSetting.tileGo,BoardSetting.tileSprite);
+    }
+
+}
